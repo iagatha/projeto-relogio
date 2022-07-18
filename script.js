d6f52e90bfde1af7d0b03cf072636e1258123b66
Iagatha de Souza <iagathasouzaa@gmail.com>
2022-07-18 11:00:59 -0300

    projeto-relogio

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function atualizarTempo() {
+    var agora = new Date();
+
+    var horario = corrigirHorario(agora.getHours()) + ":" + corrigirHorario(agora.getMinutes()) + ":" + corrigirHorario(agora.getSeconds());
+
+    var display = document.querySelector(".display");
+    display.textContent = horario;
+
+}
+
+function corrigirHorario(numero){
+    if(numero < 10){
+        numero = ")" + numero
+    }
+    return numero
+}
+
+atualizarTempo();
+setInterval(atualizarTempo, 1000);
